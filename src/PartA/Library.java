a567800f8497fef570c3e437af7cef686ae740fe
xvibngn <nguby029@mymail.unisa.edu.au>
2024-10-21 23:07:04 +1030

    Update Library.java
    Added methods to add books and multimedia items to the library.

--- src/PartA/Library.java
+++ src/PartA/Library.java
@@ -9,4 +9,15 @@ public class Library {
         this.books = new ArrayList<>();
         this.multimediaItems = new ArrayList<>();
     }
+    
+ // Add a book to the library
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+ // Add a multimedia item to the library
+    public void addMultimedia(Multimedia item) {
+        multimediaItems.add(item);
+    }
+
 }
